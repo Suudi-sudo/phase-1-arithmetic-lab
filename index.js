ed18a64cb56fa65995a46ccc44db3490e860b9ff
suudi abdisalan <abdisalansucdi@gmail.com>
2024-09-24 15:04:20 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
+let random = 5
+console.log(random)
+
+let num3 = 12 
+let num4 = 8
+let mod = num3 % num4
+console.log(mod)
+
+//let Numbers = [10,14,20,6,8] ;
+//let max = math.max(Numbers);
+//console.log(max)
+const max =20
+console.log(max)
